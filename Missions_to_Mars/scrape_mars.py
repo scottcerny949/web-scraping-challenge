01d8d839342676635d7f830da471f54d38605a65
Scott Cerny <scottcerny@yahoo.com>
2021-06-15 13:21:40 -0700

    added scrape_mars.py file

---
+++ Missions_to_Mars/scrape_mars.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+import requests
+import os
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
